12edbe4106721bc17a37b2331e40f79189b4c01a
ichisann13 <571238614@qq.com>
2024-07-14 15:26:57 +0800

    需要返回翻译结果

--- translator.py
+++ translator.py
@@ -2,7 +2,6 @@
 
 import requests
 import random
-import json
 from hashlib import md5
 
 def make_md5(s, encoding='utf-8'):
@@ -26,38 +25,38 @@ def init_key():
 
     return appid, appkey
 
-def translator():    
-    appid, appkey = init_key()
-
-    # 自动检测auto, 英文en, 中文zh, 日语jp
-    from_lang = 'en'
+def init_translator(source_lang):
+    # 自动检测auto, 英文en, 中文zh
+    from_lang = source_lang
     to_lang =  'zh'
     url = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
 
+    return from_lang, to_lang, url
+
+def translator(source_text, source_lang):    
+    appid, appkey = init_key()
+    from_lang, to_lang, url = init_translator(source_lang)
+
     while True:
-        query = input("输入待翻译内容: ")
+        query = source_text
+        if query == "":
+            continue
 
-        # 生成salt
-        salt = random.randint(32768, 65536)
+        else:
+            # 生成salt
+            salt = random.randint(32768, 65536)
 
-        # 计算签名值, 拼接顺序:appid, query, salt, appkey
-        sign = make_md5(appid + query + str(salt) + appkey)
+            # 计算签名值, 拼接顺序:appid, query, salt, appkey
+            sign = make_md5(appid + query + str(salt) + appkey)
 
-        # 使用post方式发送要指定Content-Type为application/x-www-form-urlencoded
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        payload = {'appid': appid, 'q': query, 'from': from_lang, 'to': to_lang, 'salt': salt, 'sign': sign}
+            # 使用post方式发送要指定Content-Type为application/x-www-form-urlencoded
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            payload = {'appid': appid, 'q': query, 'from': from_lang, 'to': to_lang, 'salt': salt, 'sign': sign}
 
-        # 发送请求
-        req = requests.post(url, params=payload, headers=headers)
-        result = req.json()
+            # 发送请求
+            req = requests.post(url, params=payload, headers=headers)
+            result = req.json()
 
-        # 取出翻译结果
-        trans_result = result["trans_result"][0]["dst"]
-        print(trans_result)
-
-
-def main():
-     translator()
-
-if __name__ == "__main__":
-     main()
+            # 取出翻译结果
+            trans_result = result["trans_result"][0]["dst"]
+            print(trans_result)
