1471541382d6810c1177226c81b9b585a8c35bc3
ichisann13 <571238614@qq.com>
2024-07-15 18:49:45 +0800

    简单粗暴的持续截图

--- screenshot.py
+++ screenshot.py
@@ -1,15 +1,18 @@
 import tkinter as tk
 import pyautogui
+import tkinter
 import time
+import ocr
 import os
-from PIL import Image
+
 
 class ScreenshotApp:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self):
+        self.root = tkinter.Tk()
         self.root.attributes('-fullscreen', True)
         self.root.attributes('-alpha', 0.2)
-        self.canvas = tk.Canvas(root, cursor="cross", bg="black")
+        self.root.wm_attributes("-topmost", True)
+        self.canvas = tk.Canvas(self.root, cursor="cross", bg="black")
         self.canvas.pack(fill=tk.BOTH, expand=True)
         self.canvas.bind('<Button-3>', self.on_exit)
         self.canvas.bind('<Button-1>', self.on_click)
@@ -18,10 +21,18 @@ class ScreenshotApp:
         self.start_x = None
         self.start_y = None
         self.rect = None
+        self.width = None
+        self.height = None
+        self.result = "test"
+        self.root.mainloop()
+                         
+    def show_root(self):
+        self.root.mainloop()  
 
     def on_exit(self, event):
         time.sleep(0.1)
         self.root.destroy()
+        exit(1)
 
     def on_click(self, event):
         self.start_x = event.x
@@ -53,20 +64,21 @@ class ScreenshotApp:
         self.take_screenshot(self.start_x, self.start_y, end_x, end_y)
 
     def take_screenshot(self, start_x, start_y, end_x, end_y):
-        width = end_x - start_x
-        height = end_y - start_y
-        screenshot = pyautogui.screenshot(region=(start_x, start_y, width, height))
+        self.width = end_x - start_x
+        self.height = end_y - start_y
+        self.save_screenshot()
+
+    def save_screenshot(self):
+        screenshot = pyautogui.screenshot(region=(self.start_x, self.start_y, self.width, self.height))
 
         if os.path.isdir("screenshots") == False:
             os.mkdir("screenshots")
 
-        save_path = "./screenshots/screenshot.png"
+        save_path = "./screenshots/scrshot.png"
         screenshot.save(save_path)
-        print(f"截图已保存到 {save_path}")
 
-def scr_shot():
-    root = tk.Tk()
-    app = ScreenshotApp(root)
-    root.mainloop()
+        self.result = ocr.ocr_func()
 
-# scr_shot()
+    def get_result(self):
+        return self.result
+    
